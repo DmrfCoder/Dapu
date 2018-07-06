22e714300da67e3c686917dbaa9484e6f9248b40
DmrfCoder <xuefanggag97@gmail.com>
2018-07-06 23:19:20 +0800

    完成了大概布局

---
+++ Source/DaPu/app/src/main/java/cn/xiaojii/dapu/Fragment/FragmentDiabetes.java
@@ -0,0 +1,25 @@
+package cn.xiaojii.dapu.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.xiaojii.dapu.R;
+
+/**
+ * Created by Carson_Ho on 16/5/23.
+ */
+public class FragmentDiabetes extends Fragment
+
+    {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+        savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_database, null);
+        return view;
+    }
+
+    }
---
+++ Source/DaPu/app/src/main/java/cn/xiaojii/dapu/MyFragmentAdapter.java
@@ -0,0 +1,34 @@
+package cn.xiaojii.dapu;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Carson_Ho on 16/5/23.
+ */
+public class MyFragmentAdapter extends FragmentPagerAdapter{
+
+    List<Fragment> fragmentList;
+
+
+    public MyFragmentAdapter(FragmentManager fm, List<Fragment> fragmentList) {
+        super(fm);
+        this.fragmentList = fragmentList;
+    }//写构造方法，方便赋值调用
+
+    @Override
+    public Fragment getItem(int arg0) {
+        return fragmentList.get(arg0);
+    }//根据Item的位置返回对应位置的Fragment，绑定item和Fragment
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }//设置Item的数量
+
+}
+
+
