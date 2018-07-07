7947df1cf389394ab91405e32257b4a9b593c3d9
DmrfCoder <xuefanggag97@gmail.com>
2018-07-07 12:58:45 +0800

    add the json files to assets,add readn json

---
+++ Source/DaPu/app/src/main/java/cn/xiaojii/dapu/Bean/QuestionnaireAndTestBean.java
@@ -0,0 +1,11 @@
+package cn.xiaojii.dapu.Bean;
+
+import java.util.List;
+
+public class QuestionnaireAndTestBean {
+    private List<String> AnswerCandidate;
+    private String Chapter;
+    private String Index;
+    private String Question;
+
+}
---
+++ Source/DaPu/app/src/main/java/cn/xiaojii/dapu/Util/ParseJsonUtil.java
@@ -0,0 +1,44 @@
+package cn.xiaojii.dapu.Util;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import cn.xiaojii.dapu.Bean.QuestionnaireAndTestBean;
+
+public class ParseJsonUtil {
+    private ReadFileToStringUtil readFileToStringUtil;
+    private List<QuestionnaireAndTestBean> QuestionnaireAndTestBeanList;
+
+
+    public ParseJsonUtil(Context context) {
+        readFileToStringUtil = new ReadFileToStringUtil(context);
+
+    }
+
+    public List<QuestionnaireAndTestBean> GetJsonData(String filename) {
+        String json = readFileToStringUtil.ReadJsonToString(filename + ".json");
+        Gson gson = new Gson();
+        List<JsonElement> list = new ArrayList();
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement = jsonParser.parse(json);  //将json字符串转换成JsonElement
+        JsonArray jsonArray = jsonElement.getAsJsonArray();  //将JsonElement转换成JsonArray
+        Iterator it = jsonArray.iterator();  //Iterator处理
+        QuestionnaireAndTestBeanList = new ArrayList<>();
+
+        while (it.hasNext()) {  //循环
+            jsonElement = (JsonElement) it.next(); //提取JsonElement
+            json = jsonElement.toString();  //JsonElement转换成String
+            QuestionnaireAndTestBean questionnaireAndTestBean = gson.fromJson(json, QuestionnaireAndTestBean.class); //String转化成JavaBean
+            QuestionnaireAndTestBeanList.add(questionnaireAndTestBean);
+        }
+        return QuestionnaireAndTestBeanList;
+    }
+}
