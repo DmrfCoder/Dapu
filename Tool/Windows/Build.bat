a139e318ad88e0d753507af39b6265599a273788
James Mao <maozuqiu@gmail.com>
2018-07-11 00:06:51 +0800

    fix buckup

--- Tool/Windows/Build.bat
+++ Tool/Windows/Build.bat
@@ -1,8 +1,8 @@
 
 echo "1. build"
 
-set rw=%~dp0../../
-cd %rw%
+set RootPath=%~dp0../../
+cd %RootPath%
 
 set SOURCE_PATH=%RootPath%Source/DaPu/app/build/outputs/apk/debug/app-debug.apk
 del %SOURCE_PATH%
