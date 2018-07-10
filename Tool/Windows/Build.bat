9ed94a0b8a2e681356c13a1dad86e4f1f5876893
James Mao <maozuqiu@gmail.com>
2018-07-11 00:14:19 +0800

    fix buckup

--- Tool/Windows/Build.bat
+++ Tool/Windows/Build.bat
@@ -5,8 +5,8 @@ set RootPath=%~dp0../../
 cd %RootPath%
 
 set SOURCE_PATH=%RootPath%Source/DaPu/app/build/outputs/apk/debug/app-debug.apk
-del %SOURCE_PATH%
+del "%SOURCE_PATH%" /s
 
 cd Source/DaPu
-gradlew build
+call  gradlew build
 cd %RootPath%Tool/Windows
